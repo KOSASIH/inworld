3fdc3e83cea289e9d4c6b1f19396b4956fb22ae5
KOSASIH <kosasihg88@gmail.com>
2022-05-01 12:52:15 +0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <div>
       <h1>Hello Forest Saver !</h1>
-      <p>Welcome to InWorld together we save our forest.</p>
+      <p>Welcome to InWorld, together we save our forest. 
+        Forests are an important part of our lives. Forests provide a variety of our needs as a support in carrying out activities to carry out life.
+      For this reason, InWorld invites all to work together to protect and preserve the forests around us. So that our children and grandchildren can feel and can also get all the supporting necessities of life provided by the forest.
+      </p>
     </div>
   );
 }
